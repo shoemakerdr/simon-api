93b1e8aa1504769d4aa8bd0e6baf9e33ecfab988
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-01 13:01:34 -0500

    Started creating ideal API in test.

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+
+// Sample API
+
+	/**
+	 @param {Number} possible
+	 @param {Number} seriesLength (optional)
+	 */
+const simon = new Simon(4, 20);
+
+simon.startSeries();
+simon.getSeries()
+simon.getNext();
+//
