79067ce65aeec4bb0c13faa933d68f9b2fccc3f6
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-09 13:42:11 -0500

    Added guesses property to check class

--- check.js
+++ check.js
@@ -4,12 +4,15 @@ class Check {
 	constructor(answers) {
 		this.answers = answers
 		this.iterAnswers = answers.entries()
+        this.guesses = 0
 	}
 	guess (possible) {
+        this.guesses++
 		return possible === this.iterAnswers.next().value[1]
 	}
 
 	reset () {
+        this.guesses = 0
 		this.iterAnswers = this.answers.entries()
 	}
 }
